9cd9dcbc50ad5238826f7eb7f7221ac53386158c
TheMarlboroMan <marlborometal@gmail.com>
2018-05-23 21:49:10 +0200

    Adding multipart stuff

--- main.php
+++ main.php
@@ -3,24 +3,42 @@ ini_set('display_errors', 1);
 ini_set('error_reporting', -1);
 include "autoload.php";
 
-try {
-	$request=\tools\request::from_apache_request();
-	print_r($request);
 
-//	echo $_SERVER['QUERY_STRING'].' vs '.$request->get_query_string();
-//	print_r($_GET);
-//	print_r($request->get_query_string_form());
-}
-catch(\Exception $e) {
-	die("ERROR:".$e->getMessage()."\n");
-}
+if(!isset($_POST['userid'])) {
 
-/*
-echo <<<R
-<form method="get" action="">
-	<input type="text" name="hi[]" value="a" />
-	<input type="text" name="hi[]" value="b" />
-	<input type="submit" />
-</form>
+	echo <<<R
+	<form method="POST" action="main.php" enctype="multipart/form-data">
+		<input type="text" name="userid" value="1" />
+		<input type="text" name="filecomment" value="This is an image file" />
+		<input type="file" name="data" />
+		<input type="submit" />
+	</form>
+
+	<form method="POST" action="main.php">
+		<input type="text" name="userid" value="1" />
+		<input type="text" name="filecomment" value="This is an image file" />
+		<input type="submit" />
+	</form>
 R;
-*/
+	die();
+}
+else {
+	try {
+
+	//echo "INPUT: ".PHP_EOL.file_get_contents('php://input').PHP_EOL;
+	//echo "STDIN: ".file_get_contents('php://stdin').PHP_EOL;
+	//print_r(getallheaders());
+	//print_r($_SERVER);
+		$request=\tools\request::from_apache_request();
+//		die($request->get_body()).'loool';
+//		print_r($request);
+		print_r($request->get_body_form());
+
+	//	echo $_SERVER['QUERY_STRING'].' vs '.$request->get_query_string();
+	//	print_r($_GET);
+	//	print_r($request->get_query_string_form());
+	}
+	catch(\Exception $e) {
+		die("ERROR:".$e->getMessage()."\n");
+	}
+}
--- src/tools/request.php
+++ src/tools/request.php
@@ -13,6 +13,34 @@ class request_exception_no_cli extends request_exception {
 	public function __construct() {parent::__construct("cannot create web request in cli mode. Use alternative constructors.");}
 };
 
+class body {
+
+	private $headers=[];
+	private $body=null;
+	private $name=null; //After content disposition, which is always form-data.
+//	private $is_a_file=null; //TODO: public function is_file();
+	private $filename=null;
+
+/*
+------------------------1cd9f201078b387e
+Content-Disposition: form-data; name="userid"
+
+1
+------------------------1cd9f201078b387e
+Content-Disposition: form-data; name="filecomment"
+
+This is an image file
+------------------------1cd9f201078b387e
+Content-Disposition: form-data; name="data"; filename="hola.txt"
+Content-Type: text/plain
+
+HOLA
+
+*/	
+
+	//TODO.
+}
+
 class request {
 
 	private	$status=null;
@@ -20,6 +48,7 @@ class request {
 	private $uri=null;
 	private $query_string=null;
 	private	$body=null;
+	private $bodies=[];	//Multiple body parts, you see...
 	private	$headers=[];
 
 	public static function	from_apache_request() {
@@ -28,18 +57,49 @@ class request {
 			throw new request_exception_no_cli;
 		}
 
-		return new request($_SERVER['REQUEST_METHOD'], 
+		$headers=getallheaders();
+		$method=$_SERVER['REQUEST_METHOD'];
+
+		return new request($method, 
 			$_SERVER['REQUEST_URI'],
 			$_SERVER['SERVER_PROTOCOL'],
-			getallheaders(),
-			file_get_contents('php://input'));
+			$headers,
+			self::can_get_body_from_input($headers, $method) ? file_get_contents('php://input') : self::raw_body_from_php_parsed_data($_POST, $_FILES, $headers));
 	}
 
 	public function	get_method() {return $this->method;}
+
 	public function get_query_string() {return $this->query_string;}
-	public function	get_body() {return $this->body;}
-	public function get_query_string_form() {return self::as_query_string($this->query_string);}
+
+	//TODO: ACTUALLY: SEPARATE IN TWO: THE MULTIPART AND THE NON MULTIPART....
+
+	//TODO: What about multiple bodies???.
+	public function	get_body() {
+		if($this->is_multipart()) {
+			throw new request_exception('cannot access single body for multipart requests');
+		}
+		return $this->body;
+	}
+
+	//TODO: What about multiple bodies???
 	public function get_body_form() {return self::as_query_string($this->body);}
+
+	//TODO: public function get_bodies() {} //Throw if not multipart.
+	//TODO: public function get_body_index($v) {} //Throw if not multipart.
+
+	public function get_query_string_form() {
+		if($this->is_multipart()) {
+			throw new request_exception('cannot access single body for multipart requests');
+		}
+		return self::as_query_string($this->query_string);
+	}
+
+
+
+	public function is_multipart() {
+		//TODO: Mind the casing.
+		return isset($this->headers['Content-Type']) && false!==strpos($this->headers['Content-Type'], 'multipart/form-data');
+	}
 
 	private function __construct($_method, $_uri, $_protocol, array $_headers, $_body) {
 
@@ -48,9 +108,22 @@ class request {
 		$this->query_string=self::query_string_from_uri($this->uri);
 		$this->status="{$_method} {$_uri} {$_protocol}";
 		$this->headers=$_headers;
-		$this->body=$_body;
+
+		if(this->is_multipart()) {
+			die('sorry, multipart/form-data is not yet implemented!');
+			//TODO: Each body part should be a instance of "body", with headers and shit.
+			//TODO: A facility method for compacting non-file multipart bodies into a query string could be added.
+			//TODO: A facility method for compacting multipart files into other data could be added.
+			//TODO: $body should be left to null and throw if accessed.
+		}
+		else {
+			$this->body=$_body;
+		}
 	}
 
+	////////////////////////////////////////////////////////////////////////
+
+	//!Obtains the query string part from the URI. Might as well get it from $_SERVER.
 	private static function query_string_from_uri($_uri) {
 
 		if(false===strpos($_uri, '?')) {
@@ -61,10 +134,61 @@ class request {
 		}
 	}
 
+	//Returns $_str as a query string, useful to parse post or get data to familiar PHP forms.
 	private static function as_query_string($_str) {
 
 		$result=[];
 		parse_str($_str, $result);
 		return $result;
+	}
+
+	//!Body cannot be retrieved from php://input when there is a multipart/form-data content type in a post request.
+	private static function can_get_body_from_input(array $_headers, $method) {
+
+		//TODO: What about headers upper and lowercasing...
+		return ! ('POST'===strtoupper($method) && isset($_headers['Content-Type']) && false!==strpos($_headers['Content-Type'], 'multipart/form-data'));
+	}
+
+	//!Converts the post and files superglobals into their original raw forms.
+	private static function raw_body_from_php_parsed_data($_post, $_files, $_headers) {
+
+		//TODO: Mind the letter casing.
+		$content_type=$_headers['Content-Type'];
+		//Lol... Explode the header by ;, the second part is the boundary=xxxx part. Explode that by = and return the second part.
+		$boundary=trim(explode('=', explode(';',$_headers['Content-Type'], 2)[1])[1]);
+
+		//First the post data...
+		$post_data=null;
+		foreach($_post as $k => $v) {
+			$post_data.=<<<R
+{$boundary}
+Content-Disposition: form-data; name="{$k}"
+
+{$v}
+
+R;
+		}
+
+		//TODO: What about invalid files???
+		//TODO: Try sending an empty file!.
+
+		$file_data=null;
+		foreach($_files as $k => $v) {
+			$file_body=file_get_contents($v['tmp_name']);
+			$file_data.=<<<R
+{$boundary}
+Content-Disposition: form-data; name="{$k}"; filename="{$v['name']}"
+Content-Type: {$v['type']}
+
+{$file_body}
+
+R;
+		}
+
+		$raw_body=<<<R
+{$post_data}{$file_data}{$boundary}--
+R;
+
+		return $raw_body;
 	}
 };
